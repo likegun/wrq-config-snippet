4d0acda2ccafb0397a8811d1a7bb7a035fa5fb39
wangrq <likegun94@gmail.com>
2017-08-04 11:19:36 +0800

    initial commit

---
+++ spec/wrq-config-snippet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WrqConfigSnippetView from '../lib/wrq-config-snippet-view';
+
+describe('WrqConfigSnippetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
